e08cbc264bde614864f9515ee497f3001965097b
Johannes Millan <johannes.millan@gmail.com>
2015-03-13 23:08:17 +0100

    prepared example

--- example/scripts/example.js
+++ example/scripts/example.js
@@ -2,10 +2,61 @@ angular.module('exampleApp', [
     'angularPromiseButtons',
     'ngAnimate'
 ])
-    .controller('exampleCtrl', function ($scope)
+    .factory('fakeFac', function ($q, $timeout, $log)
     {
-        $scope.submit = function ()
-        {
-            alert('Form submitted');
+        var standardDelay = 3000;
+        return {
+            success: function ()
+            {
+                var defer = $q.defer();
+                $timeout(function ()
+                {
+                    $log.info('resolve');
+                    defer.resolve({
+                        msg: 'SUCCESS'
+                    })
+                }, standardDelay);
+                return defer.promise;
+            },
+            error: function ()
+            {
+                var defer = $q.defer();
+                $timeout(function ()
+                {
+                    $log.info('error');
+                    defer.reject({
+                        msg: 'ERROR'
+                    })
+                }, standardDelay);
+                return defer.promise;
+            },
+            endless: function ()
+            {
+                var defer = $q.defer();
+                return defer.promise;
+            }
         };
+    })
+    .controller('exampleCtrl', function ($scope, fakeFac)
+    {
+        $scope.success = function ()
+        {
+            $scope.successPromise = fakeFac.success();
+        };
+
+        $scope.error = function ()
+        {
+            $scope.errorPromise = fakeFac.error();
+        };
+
+        $scope.endlesse = function ()
+        {
+            $scope.endlessePromise = fakeFac.endlesse();
+        };
+
+        $scope.auto = function ()
+        {
+            $scope.autoPromise = fakeFac.endless();
+        };
+        $scope.auto();
     });
