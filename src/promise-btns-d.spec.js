10a8240e97cb046af186395e8fe0f3b0329e6130
Johannes Millan <johannes.millan@gmail.com>
2015-12-25 18:27:44 +0100

    #7 add unit tests for returning a promise directly from function for ngSubmit

--- src/promise-btns-d.spec.js
+++ src/promise-btns-d.spec.js
@@ -69,7 +69,7 @@ describe('promise-buttons directive', function ()
 
         beforeEach(function ()
         {
-            var html = '<button class="btn" ng-click="asyncCall()" promise-btn="promise">Success after delay</button>';
+            var html = '<button ng-click="asyncCall()" promise-btn="promise">Success after delay</button>';
             element = $compile(html)(scope);
             scope.$digest();
             scope.asyncCall = function ()
@@ -214,7 +214,7 @@ describe('promise-buttons directive', function ()
 
         beforeEach(function ()
         {
-            var html = '<button class="btn" ng-click="asyncCall()" promise-btn="promise">Success after delay</button>';
+            var html = '<button ng-click="asyncCall()" promise-btn="promise">Success after delay</button>';
             element = $compile(html)(scope);
             scope.$digest();
         });
@@ -306,7 +306,7 @@ describe('promise-buttons directive', function ()
 
         beforeEach(function ()
         {
-            var html = '<button class="btn" ng-click="asyncCall()" promise-btn>Success after delay</button>';
+            var html = '<button ng-click="asyncCall()" promise-btn>Success after delay</button>';
             element = $compile(html)(scope);
             scope.$digest();
             scope.asyncCall = function ()
@@ -440,6 +440,157 @@ describe('promise-buttons directive', function ()
             $timeout.flush();
             expect(element.hasClass('is-loading')).toBeTruthy();
             expect(element.attr('disabled')).toBe('disabled');
+        }));
+    });
+
+
+    describe('a promise directly returned by form ngSubmit function', function ()
+    {
+        var btnEl;
+        var formEl;
+
+        beforeEach(function ()
+        {
+            var html = '<form ng-submit="asyncCall()" promise-btn>' +
+                '<button type="submit">Success after delay</button>' +
+                '</form>';
+
+            formEl = $compile(html)(scope);
+            btnEl = formEl.children()[0];
+            btnEl = angular.element(btnEl);
+
+            scope.$digest();
+            scope.asyncCall = function ()
+            {
+                return fakeFact.success();
+            };
+        });
+
+        it('should have a spinner appended', function ()
+        {
+            expect(angular.element(btnEl.find('span')[0]).hasClass('btn-spinner'))
+                .toBeTruthy();
+        });
+
+        it('has the is-spinning class appended on click', function ()
+        {
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+        });
+
+        it('is disabled on click', function ()
+        {
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.attr('disabled')).toBe('disabled');
+        });
+
+        it('is not disabled after promise is resolved', function ()
+        {
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.attr('disabled')).toBe('disabled');
+            $timeout.flush();
+            expect(btnEl.attr('disabled')).not.toBe('disabled');
+        });
+
+        it('hasn\'t the is-spinning after promise is resolved', function ()
+        {
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+            $timeout.flush();
+            expect(btnEl.hasClass('is-loading')).toBeFalsy();
+        });
+
+        it('should work the same with response errors', function ()
+        {
+            scope.asyncCall = function ()
+            {
+                return fakeFact.error();
+            };
+
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+            expect(btnEl.attr('disabled')).toBe('disabled');
+            $timeout.flush();
+            expect(btnEl.hasClass('is-loading')).toBeFalsy();
+            expect(btnEl.attr('disabled')).not.toBe('disabled');
+        });
+
+        it('should always be disabled and has class is loading for unresolvable requests', function ()
+        {
+            scope.asyncCall = function ()
+            {
+                return fakeFact.endless();
+            };
+
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+            expect(btnEl.attr('disabled')).toBe('disabled');
+            $timeout.flush();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+            expect(btnEl.attr('disabled')).toBe('disabled');
+        });
+
+        it('should be able to handle promise chains', function ()
+        {
+            scope.asyncCall = function ()
+            {
+                scope.v = {promiseIndex: 0};
+                return scope.countChain()
+                    .then(scope.countChain)
+                    .then(scope.countChain)
+                    .then(scope.countChain)
+                    .then(scope.countChain);
+            };
+            scope.countChain = function ()
+            {
+                return fakeFact.success().then(function ()
+                {
+                    scope.v.promiseIndex++;
+                    if (scope.v.promiseIndex < 5) {
+                        expect(btnEl.hasClass('is-loading')).toBeTruthy();
+                    }
+                });
+            };
+
+            expect(btnEl.hasClass('is-loading')).toBeFalsy();
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+
+            // test test
+            $timeout.flush();
+            expect(scope.v.promiseIndex).toBe(5);
+            expect(btnEl.hasClass('is-loading')).toBeFalsy();
+        });
+
+        it('should work with $resource promises', inject(function ($q)
+        {
+            var mockBagelApiService = {
+                query: function ()
+                {
+                    var queryDeferred = $q.defer();
+                    return {$promise: queryDeferred.promise};
+                }
+            };
+            scope.asyncCall = function ()
+            {
+                return mockBagelApiService.query();
+            };
+
+
+            formEl.triggerHandler('submit');
+            scope.$digest();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+            expect(btnEl.attr('disabled')).toBe('disabled');
+            $timeout.flush();
+            expect(btnEl.hasClass('is-loading')).toBeTruthy();
+            expect(btnEl.attr('disabled')).toBe('disabled');
         }));
     });
 });
