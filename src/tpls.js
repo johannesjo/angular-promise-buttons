8a8bc333bc59253dff0630839f2cb4bc75548292
Johannes Millan <johannes.millan@gmail.com>
2015-12-25 18:29:03 +0100

    #7 remove template, as we don't need it any more

--- src/tpls.js
+++
@@ -1,8 +0,0 @@
-angular.module('angularPromiseButtons').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('promise-btn-d.html',
-    "<button ng-transclude></button>"
-  );
-
-}]);
