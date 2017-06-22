c487f7c20a542c13a4e24393916da309bcb3cd38
Johannes Millan <johannes.millan@gmail.com>
2017-06-22 20:00:10 +0200

    add code formatting

--- karma.conf.js
+++ karma.conf.js
@@ -1,72 +1,62 @@
 // Karma configuration
 // Generated on Fri Aug 01 2014 09:51:05 GMT+0200 (CEST)
 
-module.exports = function (config)
-{
-    config.set({
+module.exports = function(config) {
+  config.set({
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+    // list of files / patterns to load in the browser
+    files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
+      // bower:js
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/es6-promise/es6-promise.js',
+      // endbower
+      'bower_components/es6-promise/es6-promise.auto.js',
 
+      // modules first
+      'src/**/_*.js',
+      // all the rest of the files
+      'src/**/*.js'
+    ],
 
-        // list of files / patterns to load in the browser
-        files: [
-            './node_modules/phantomjs-polyfill/bind-polyfill.js',
-            // bower:js
-            'bower_components/angular/angular.js',
-            'bower_components/angular-animate/angular-animate.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/es6-promise/es6-promise.js',
-            // endbower
-            'bower_components/es6-promise/es6-promise.auto.js',
+    // list of files to exclude
+    exclude: [],
 
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      '**/src/**/!(*.spec)+(.js)': ['coverage']
+    },
 
-            // modules first
-            'src/**/_*.js',
-            // all the rest of the files
-            'src/**/*.js'
-        ],
+    // web server port
+    port: 9876,
 
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-        // list of files to exclude
-        exclude: [],
-
-        preprocessors: {
-            // source files, that you wanna generate coverage for
-            // do not include tests or libraries
-            // (these files will be instrumented by Istanbul)
-            '**/src/**/!(*.spec)+(.js)': ['coverage']
-        },
-
-        // web server port
-        port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+    autoWatch: true,
 
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
-    });
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
 };
--- wallaby.js
+++ wallaby.js
@@ -1,28 +1,27 @@
 // wallaby.conf.js
 // for frontend
 
-module.exports = function ()
-{
-    return {
-        files: [
-            // bower:js
-            'bower_components/angular/angular.js',
-            'bower_components/angular-animate/angular-animate.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/es6-promise/es6-promise.js',
-            // endbower
-            'bower_components/es6-promise/es6-promise.auto.js',
+module.exports = function() {
+  return {
+    files: [
+      // bower:js
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/es6-promise/es6-promise.js',
+      // endbower
+      'bower_components/es6-promise/es6-promise.auto.js',
 
-            // modules first
-            'src/**/_*.js',
-            // all the rest of the files
-            'src/**/*.js',
-            // exclude tests
-            {pattern: 'src/**/*spec.js', ignore: true}
-        ],
-        tests: [
-            'src/**/*spec.js'
-        ],
-        preprocessors: {}
-    }
+      // modules first
+      'src/**/_*.js',
+      // all the rest of the files
+      'src/**/*.js',
+      // exclude tests
+      { pattern: 'src/**/*spec.js', ignore: true }
+    ],
+    tests: [
+      'src/**/*spec.js'
+    ],
+    preprocessors: {}
+  }
 };
